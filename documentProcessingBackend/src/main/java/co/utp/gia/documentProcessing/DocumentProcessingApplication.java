4f1683c1e35fa7715a76fff07e1fdd465a0ed4b5
Alejandro Vergara <alejandro.vergara@utp.edu.co>
2022-07-07 10:33:09 -0500

    create Spring Boot app

---
+++ documentProcessingBackend/src/main/java/co/utp/gia/documentProcessing/DocumentProcessingApplication.java
@@ -0,0 +1,13 @@
+package co.utp.gia.documentProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentProcessingApplication.class, args);
+	}
+
+}
---
+++ documentProcessingBackend/src/test/java/co/utp/gia/documentProcessing/DocumentProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.utp.gia.documentProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
